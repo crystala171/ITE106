bc96b927c1ba2105e3815e4f48e4e9340de3c7f5
Crystal Jane E. Ubaña <crystaljaneubana@gmail.com>
2024-11-14 16:12:10 +0800

    Create PrintName.java

---
+++ PrintName.java
@@ -0,0 +1,12 @@
+import myPackage.myClass;
+
+public class PrintName
+{
+	public static void main(String args[])
+	{
+		String name = "GeeksForGeeks";
+		myClass obj = new myClass();
+		
+		obj.getNames(name);
+	}
+}
